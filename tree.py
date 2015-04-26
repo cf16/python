4258838b945cf278868e41f4479bc5b2fcb7e39b
cf16 <cf16rocket@gmail.com>
2015-04-26 21:56:17 +0100

    python.........

--- tree.py
+++ tree.py
@@ -4,35 +4,41 @@ import math
 class Vertex():
    prev = ""
    id = ""
-   def __init__(self,id,prev):
+   def __init__(self,prev,id):
        self.prev = prev
        self.id = id
    #def area(self):
    #    s = 0.5*(self.a+self.b+self.c)
    #    return math.sqrt(s*(s-self.a)*(s-self.b)*(s-self.c))
 
-def path_to_root(v):
+def path_to_root(id,vertices_list):
     # print path up to root vertex
     path = list()
+    path.append(id)
     while 1:
         # print(id)
-        path.append(v.id)
-        if v.prev == "":
-            break
+        ## path.append(it.id)
+        #if v.prev == "":
+        #    break
         # find vertex with id.prev
         found = False
-        for v in vertices_list:
-            if v.id == id.prev:
-                # found
+        for it in vertices_list:
+            if it.id == id:
+                # found, it is vertex with this id
                 found = True
                 break
         if found == False:
-            raise ValueError('path_to_root: Index not found!')
+            # not found, id is root index
+            #path.append(it.prev)
+            break
+        id = it.prev
+        path.append(id)
+    return path
 
 def tree_create(vertices_list,out):
     # create vertices and insert into out list        
     for i in range (0,len(vertices_list)):
-        out.append(vertices_list[i])
+        out.append(Vertex(vertices_list[i][0],vertices_list[i][1]))
 
 # data
 vertices = list()
@@ -42,14 +48,14 @@ tree_in = eval(input())
 query_in = eval(input())
 
 # print
-print("tree_in: ")
-print(tree_in)
-print("query_in: ")
-print(query_in)
+#print("tree_in: ")
+#print(tree_in)
+#print("query_in: ")
+#print(query_in)
 print("\n")
 # get vertices list
 tree_create(tree_in,vertices)
-print(vertices)
-for v in query_in:
-    
+#print(vertices)
+for id in query_in:
+    print(path_to_root(id,vertices))
 
